7a0c4c1bac6ed48a36ac07aa9bdc251609ee0882
jundolescent <houl0310@gmail.com>
2023-11-23 12:07:19 +0900

    fix

---
+++ startfabcar.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash
+
+ npx caliper launch manager --caliper-workspace ./caliper-benchmarks --caliper-networkconfig networks/networkConfig.yaml --caliper-benchconfig benchmarks/samples/fabric/fabcar/config.yaml --caliper-flow-only-test --caliper-fabric-gateway-enabled
