fc355e3cc3eb99350ebf43db1aa070ed42bd2123
jundolescent <houl0310@gmail.com>
2023-11-24 12:25:04 +0900

    fix caliper

--- caliper-configure.py
+++ caliper-configure.py
@@ -31,7 +31,7 @@ for org in range(2, n_org + 1):
     temp_org = {'mspid': 'Org{}MSP'.format(org), \
                 'identities': {'certificates': [{'name': 'User1', 'clientPrivateKey':{'path':'../organizations/peerOrganizations/org{}.example.com/users/User1@org{}.example.com/msp/keystore/priv_sk'.format(org,org)},\
                                                  'clientSignedCert':{'path':'../organizations/peerOrganizations/org{}.example.com/users/User1@org{}.example.com/msp/signcerts/User1@org{}.example.com-cert.pem'.format(org,org,org)}}]}, \
-                'connectionProfile': {'path':'../organizations/peerOrganizations/org{}.example.com/connection-org{}.yaml'.format(org, org), 'discover': True}}
+                'connectionProfile': {'path':'../organizations/peerOrganizations/org{}.example.com/connection-org{}.json'.format(org, org), 'discover': True}}
     network_config['organizations'].append(temp_org)
 
 
@@ -54,99 +54,78 @@ if os.path.isfile('caliper.yaml'):
 
 ##### generate connection profile ########
 
+# for i in range(0, n_server):
+#     for node in deployment['Deployment']['deployment']['configuration']:
+#         # peer0.org1, peer1.org1, orderer ...
+#         ip = deployment['Deployment']['deployment'][i]['ip']
+#         port_list = []
+#         if 'peer' in node:
+#             peer, org = node.split('.')
+#             peer_port = 8000 + int(org[0]) * 100 + int(peer[-1])
+#             port_list.append(peer_port)
+#             #only IP changes, yet keep in mind that the number of peer could be variable
+#     os.system('./generateccp.sh {} {} {} {} {}'.format())
 
 
+#only IP changes, yet keep in mind that the number of peer could be variable
+print(server_ip)
+for o in range(1, n_org + 1):
+    peer_name = ''
+    peer_port = 0
+    port_list = []
+    ca_port = 0
+    for p in range(0, n_peer):
+        peer_name = 'peer{}.org{}'.format(o, p)
+        peer_port = 8000 + o * 100 + p
+        port_list.append(peer_port)
+        ca_port = 10000 + o
+    os.system('./generateccp.sh {} {} {} {} {}'.format(server_ip[peer_name], o, port_list[0], ca_port, port_list[1]))
 
 
-#ip, PEER_PORT, peerpem,CAPEM, ca_port
+# #ip, PEER_PORT, peerpem,CAPEM, ca_port
 
 
-for org in range(1, n_org + 1):
-    with open('./ccp-template.yaml') as f:
-        ccp_config = yaml.load(f, Loader=yaml.FullLoader)
-    f2 = open('organizations/peerOrganizations/org{}.example.com/tlsca/tlsca.org{}.example.com-cert.pem'.format(org,org),'r')
-    peerpem = f2.readlines()
-    pem = ''
-    for sen in peerpem:
-        pem = pem + sen
-    f2.close()
-    print(peerpem)
-    f3 = open('organizations/peerOrganizations/org{}.example.com/ca/ca.org{}.example.com-cert.pem'.format(org,org),'r')
-    capem = f3.readlines()
-    f3.close()
-    print(capem)
+# for org in range(1, n_org + 1):
+#     with open('./ccp-template.yaml') as f:
+#         ccp_config = yaml.load(f, Loader=yaml.FullLoader)
+#     f2 = open('organizations/peerOrganizations/org{}.example.com/tlsca/tlsca.org{}.example.com-cert.pem'.format(org,org),'r')
+#     peerpem = f2.readlines()
+#     pem = ''
+#     for sen in peerpem:
+#         pem = pem + sen
+#     f2.close()
+#     print(peerpem)
+#     f3 = open('organizations/peerOrganizations/org{}.example.com/ca/ca.org{}.example.com-cert.pem'.format(org,org),'r')
+#     capem = f3.readlines()
+#     f3.close()
+#     print(capem)
 
-    ccp_config['name'] = 'test-network-org{}'.format(org)
-    ccp_config['client']['organization'] = 'Org{}'.format(org)
-    ccp_config['organizations'] = {'Org{}'.format(org):{'mspid': 'Org{}MSP'.format(org),\
-                                                        'peers': [],\
-                                                        'certificateAuthorities':['ca.org{}.example.com'.format(org)]}}
-    ###just do in from the scratch...
-    if org != 1:
-        del ccp_config['peers']['peer0.org1.example.com']
-        del ccp_config['certificateAuthorities']['ca.org1.example.com']
+#     ccp_config['name'] = 'test-network-org{}'.format(org)
+#     ccp_config['client']['organization'] = 'Org{}'.format(org)
+#     ccp_config['organizations'] = {'Org{}'.format(org):{'mspid': 'Org{}MSP'.format(org),\
+#                                                         'peers': [],\
+#                                                         'certificateAuthorities':['ca.org{}.example.com'.format(org)]}}
+#     ###just do in from the scratch...
+#     if org != 1:
+#         del ccp_config['peers']['peer0.org1.example.com']
+#         del ccp_config['certificateAuthorities']['ca.org1.example.com']
     
-    caport = 10000 + org
-    for peer in range(0, n_peer):
-        IP = server_ip['peer{}.org{}'.format(peer, org)]
-        peer_port = 8000 + org * 100 + peer ## 8100, 8200. 8300
-        peer_address = 'peer{}.org{}.example.com'.format(peer, org)
-        ccp_config['organizations']['Org{}'.format(org)]['peers'].append(peer_address)
+#     caport = 10000 + org
+#     for peer in range(0, n_peer):
+#         IP = server_ip['peer{}.org{}'.format(peer, org)]
+#         peer_port = 8000 + org * 100 + peer ## 8100, 8200. 8300
+#         peer_address = 'peer{}.org{}.example.com'.format(peer, org)
+#         ccp_config['organizations']['Org{}'.format(org)]['peers'].append(peer_address)
 
-        ccp_config['peers']['peer{}.org{}.example.com'.format(peer,org)] = {'url':'grpcs://{}:{}'.format(IP, peer_port), \
-                                                                            'tlsCACerts':{'pem':'{}\n'.format(pem)}, \
-                                                                            'grpcOptions':{'ssl-target-name-override': 'peer{}.org{}.example.com'.format(peer, org),
-                                                                                            'hostnameOverride': 'peer{}.org{}.example.com'.format(peer, org)}}
+#         ccp_config['peers']['peer{}.org{}.example.com'.format(peer,org)] = {'url':'grpcs://{}:{}'.format(IP, peer_port), \
+#                                                                             'tlsCACerts':{'pem':'{}\n'.format(pem)}, \
+#                                                                             'grpcOptions':{'ssl-target-name-override': 'peer{}.org{}.example.com'.format(peer, org),
+#                                                                                             'hostnameOverride': 'peer{}.org{}.example.com'.format(peer, org)}}
 
-    ccp_config['certificateAuthorities'] = {'ca.org{}.example.com'.format(org):{'url':'http://localhost:{}'.format(caport),\
-                                                                                'caName': 'ca-org{}'.format(org),\
-                                                                                'tlsCACerts':{'pem':capem},\
-                                                                                'httpOptions':{'verify': False}}}
+#     ccp_config['certificateAuthorities'] = {'ca.org{}.example.com'.format(org):{'url':'http://localhost:{}'.format(caport),\
+#                                                                                 'caName': 'ca-org{}'.format(org),\
+#                                                                                 'tlsCACerts':{'pem':capem},\
+#                                                                                 'httpOptions':{'verify': False}}}
 
-    with open('./organizations/peerOrganizations/org{}.example.com/connection-org{}.yaml'.format(org, org), 'w') as f:
-        yaml.dump(ccp_config,f,sort_keys=False)
-# name: test-network-org${ORG}
-# version: 1.0.0
-# client:
-#   organization: Org${ORG}
-#   connection:
-#     timeout:
-#       peer:
-#         endorser: '300'
-# organizations:
-#   Org${ORG}:
-#     mspid: Org${ORG}MSP
-#     peers:
-#     - peer0.org${ORG}.example.com
-#     certificateAuthorities:
-#     - ca.org${ORG}.example.com
-# peers:
-#   peer0.org${ORG}.example.com:
-#     url: grpcs://localhost:${P0PORT1}
-#     tlsCACerts:
-#       pem: |
-#           ${PEERPEM}
-#     grpcOptions:
-#       ssl-target-name-override: peer0.org${ORG}.example.com
-#       hostnameOverride: peer0.org${ORG}.example.com
-# certificateAuthorities:
-#   ca.org${ORG}.example.com:
-#     url: https://localhost:${CAPORT}
-#     caName: ca-org${ORG}
-#     tlsCACerts:
-#       pem: 
-#         - |
-#           ${CAPEM}
-#     httpOptions:
-#       verify: false
-
-# for peer in range(1, n_peer):
-#     peer_address = 'peer{}.org${{ORG}}.example.com'.format(peer)
-#     ccp_config['organizations']['Org${{ORG}}'.format()]['peers'].append(peer_address)
-
-#     ccp_config['peers']['peer{}.org${{ORG}}.example.com'.format(peer)] = {'url':'grpcs://${{IP}}:${{P0PORT{}}}'.format(peer + 1), \
-#                                                                           'tlsCACerts':{'pem':'${{PEERPEM}}\n'.format()}, \
-#                                                                           'grpcOptions':{'ssl-target-name-override': 'peer{}.org${{ORG}}.example.com'.format(peer),
-#                                                                                          'hostnameOverride': 'peer{}.org${{ORG}}.example.com'.format(peer)}}
-#     print(ccp_config['peers']['peer{}.org${{ORG}}.example.com'.format(peer)])
-
+#     with open('./organizations/peerOrganizations/org{}.example.com/connection-org{}.yaml'.format(org, org), 'w') as f:
+#         yaml.dump(ccp_config,f,sort_keys=False)
--- generateccp.sh
+++ generateccp.sh
@@ -4,20 +4,7 @@ function one_line_pem {
     echo "`awk 'NF {sub(/\\n/, ""); printf "%s\\\\\\\n",$0;}' $1`"
 }
 
-# function json_ccp {
-#     local PP=$(one_line_pem $6)
-#     local CP=$(one_line_pem $7)
-#     sed -e "s/\${ORG}/$1/" \
-#         -e "s/\${P0PORT1}/$2/" \
-#         -e "s/\${CAPORT}/$3/" \
-#         -e "s/\${P0PORT2}/$4/" \
-#         -e "s/\${IP}/$5/" \
-#         -e "s#\${PEERPEM}#$PP#" \
-#         -e "s#\${CAPEM}#$CP#" \
-#         ccp-template.json
-# }
-
-function yaml_ccp {
+function json_ccp {
     local PP=$(one_line_pem $6)
     local CP=$(one_line_pem $7)
     sed -e "s/\${ORG}/$1/" \
@@ -27,8 +14,22 @@ function yaml_ccp {
         -e "s/\${IP}/$5/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
-        ccp-template.yaml | sed -e $'s/\\\\n/\\\n          /g'
+        ccp-template.json
 }
+
+# function yaml_ccp {
+#     local PP=$(one_line_pem $6)
+#     local CP=$(one_line_pem $7)
+#     sed -e "s/\${ORG}/$1/" \
+#         -e "s/\${P0PORT1}/$2/" \
+#         -e "s/\${CAPORT}/$3/" \
+#         -e "s/\${P0PORT2}/$4/" \
+#         -e "s/\${IP}/$5/" \
+#         -e "s#\${PEERPEM}#$PP#" \
+#         -e "s#\${CAPEM}#$CP#" \
+#         ccp-template.yaml | sed -e $'s/\\\\n/\\\n          /g'
+# }
+
 
 IP=$1
 ORG=$2
@@ -38,5 +39,5 @@ P0PORT2=$5
 PEERPEM=organizations/peerOrganizations/org${ORG}.example.com/tlsca/tlsca.org${ORG}.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org${ORG}.example.com/ca/ca.org${ORG}.example.com-cert.pem
 
-echo "$(yaml_ccp $ORG $P0PORT1 $CAPORT1 $P0PORT2 $IP $PEERPEM $CAPEM)" > organizations/peerOrganizations/org${ORG}.example.com/connection-org${ORG}.yaml
+echo "$(json_ccp $ORG $P0PORT1 $CAPORT1 $P0PORT2 $IP $PEERPEM $CAPEM)" > organizations/peerOrganizations/org${ORG}.example.com/connection-org${ORG}.json
 #echo "$(yaml_ccp $ORG $P0PORT1 $CAPORT1 $P0PORT2 $PEERPEM $CAPEM)" > organizations/peerOrganizations/org1.example.com/connection-org1.yaml
